9b4e05a361dfbff29db593ee6a930a29e719ae80
codewithbrajendra <brajendra0523@gmail.com>
2023-07-10 18:18:14 +0530

    Adding Java interview questions by Jitendra Birla

---
+++ CoreJavaInterviewQues/src/Practice/BigDecimalVsDouble.java
@@ -0,0 +1,21 @@
+package Practice;
+
+import java.math.BigDecimal;
+
+public class BigDecimalVsDouble {
+
+    public static void main(String[] args) {
+        Double a = 0.02;
+        Double b = 0.03;
+        Double c = b - a;
+
+        System.out.println("Double subtraction : "+c);
+
+        BigDecimal _a = new BigDecimal(0.02);
+        BigDecimal _b = new BigDecimal(0.03);
+        BigDecimal _c = _b.subtract(_a);
+
+        System.out.println("Bigdecimal substraction : "+_c);
+
+    }
+}
